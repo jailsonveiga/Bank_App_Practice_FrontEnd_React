d5795e169e10e819a0ac9ff158120f0af6dac329
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-15 21:21:20 -0400

    Update

--- bank_app_frontend/src/App.js
+++ bank_app_frontend/src/App.js
@@ -1,12 +1,12 @@
+import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import ListAccountComponent from "./components/ListAccountComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Testing the React configuration</h1>
-      </header>
+    <div className="container">
+      <ListAccountComponent />
     </div>
   );
 }
