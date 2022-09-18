236d07096f92a66fdab297e475d2524bfe5ad6d8
Jailson Veiga <jmdaveiga2012@hotmail.com>
2022-09-18 14:46:16 -0400

    Update

--- bank_app_frontend/src/App.js
+++ bank_app_frontend/src/App.js
@@ -2,11 +2,18 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import ListAccountComponent from "./components/ListAccountComponent";
+import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
   return (
-    <div className="container">
-      <ListAccountComponent />
+    <div>
+      <HeaderComponent />
+      <div className="container">
+        <ListAccountComponent />
+      </div>
+
+      <FooterComponent />
     </div>
   );
 }
